56dfc20333f46675b4674d060da0fe321a21d08f
mohamed.guedhami <mohamed.guedhami@odaseva.com>
2022-07-08 16:19:21 +0200

    using docker host url

--- src/test/java/steps/StepsDefinition.java
+++ src/test/java/steps/StepsDefinition.java
@@ -29,6 +29,7 @@ public class StepsDefinition {
 
     @Given("I set base url {string}")
     public void iSetBaseUrl(String endPoint) {
+        // host url
         RestAssured.baseURI = "http://0.0.0.0:9090" + endPoint;
         request = RestAssured.given();
         request.header("Content-Type", "application/json");
